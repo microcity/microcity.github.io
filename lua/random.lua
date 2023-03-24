5f416bd22a0bd59671e25946065ac6614c90fed3
sunzhuo <mixwind@gmail.com>
2023-03-24 09:35:37 +0000

    test

--- lua/random.lua
+++ lua/random.lua
@@ -1,5 +1,4 @@
-local seed = math.randomseed(2, {dist = "normal", sigma = 5})
+local seed = math.randomseed(1, {distribution = "normal", mu = 5, sigma = 3})
 for i = 1, 10 do
     print(seed:random())
 end
-print(math.random())
