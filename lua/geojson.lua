f33dfe81af494722aa0dc8fc2c99c368d4b13325
mixwind <mixwind@gmail.com>
2023-08-05 06:29:32 +0000

    test

--- lua/geojson.lua
+++ lua/geojson.lua
@@ -4,5 +4,7 @@ for i,country in ipairs(obj:getchildren()) do
     for j,land in ipairs(country:getchildren()) do
         land:setmat({color='#3480C4', opacity = 0.6})
     end
+    local data = country:getdata()
+    print(data['NAME'], ", POP:", data['POP_EST'], ", GDP:", data['GDP_MD_EST'])
 end
 scene.render()
