8374cf4394cc716e1a9d6874dac17c7595c237ce
mixwind <mixwind@gmail.com>
2023-08-10 11:27:30 +0000

    test

--- lua/geojson.lua
+++ lua/geojson.lua
@@ -1,10 +1,15 @@
 scene.setenv({camtype='ortho'})
 local obj = scene.addobj('/res/world_countries.geojson')
 for i,country in ipairs(obj:getchildren()) do
-    for j,land in ipairs(country:getchildren()) do
+    local x, y, z
+    for j, land in ipairs(country:getchildren()) do
         land:setmat({color='#3480C4', opacity = 0.6})
+        local points = land:getvertices()
+        x, y, z = points[1], points[2], points[3]
     end
     local data = country:getdata()
+    local label = scene.addobj('label', {text = data['NAME'], color = 'black', size = 2})
+    label:setpos(x, y, z)
     print(data['NAME'], ", POP:", data['POP_EST'], ", GDP:", data['GDP_MD_EST'])
 end
 scene.render()
