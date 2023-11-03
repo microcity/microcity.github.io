32139c9383cbfdc91b1dbea38be11b7d8d80ba92
mixwind <mixwind@gmail.com>
2023-11-03 18:57:01 +0000

    test

--- lua/startup.lua
+++ lua/startup.lua
@@ -1,11 +1,10 @@
 print("Hello, there!")
 local obj = scene.addobj('box')
 obj:setscale(5, 5, 5)
-local x = 1
-local y = 1
-local z = 0
+
+local x, y, z = 0, 0, 0
 while scene.render() do
-	x = x + 0.1
-	y = y + 0.1
-	obj:setrot(x, y, z)
+  x = x + 0.1
+  y = y + 0.1
+  obj:setrot(x, y, z)
 end
