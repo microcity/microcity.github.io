065498739cd171d50263380c9c66b515bf0dbcdf
Mixwind <mixwind@gmail.com>
2024-01-17 03:19:25 +0000

    update lua/startup.lua.
    Signed-off-by: Mixwind <mixwind@gmail.com>

--- lua/startup.lua
+++ lua/startup.lua
@@ -1,10 +1,10 @@
-print("Hello, there!")
-local obj = scene.addobj('box')
-obj:setscale(5, 5, 5)
+print("Hello, there!")              
+local obj = scene.addobj('box') --add 1*1*1 cube to the scene
+obj:setscale(5, 5, 5)           --scale up the cube to 5*5*5
 
-local x, y, z = 0, 0, 0
-while scene.render() do
+local x, y, z = 0, 0, 0         --initialize the rotation variable
+while scene.render() do         --render the scene and check ui
   x = x + 0.1
   y = y + 0.1
-  obj:setrot(x, y, z)
+  obj:setrot(x, y, z)           --set the rotation of the cube
 end
