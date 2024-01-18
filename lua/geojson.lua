80513df00ae58cdc95968b8216b0b6c6dd2bbd52
Mixwind <mixwind@gmail.com>
2024-01-18 01:26:50 +0000

    update lua/geojson.lua.
    Signed-off-by: Mixwind <mixwind@gmail.com>

--- lua/geojson.lua
+++ lua/geojson.lua
@@ -1,15 +1,15 @@
-scene.setenv({camtype='ortho'})
-local obj = scene.addobj('/res/world_countries.geojson')
-for i,country in ipairs(obj:getchildren()) do
+scene.setenv({camtype='ortho'})                                                             --set the scene camera to orthographic
+local obj = scene.addobj('/res/world_countries.geojson')                                    --get gis data of world countries
+for i,country in ipairs(obj:getchildren()) do                                               --traverse each country
     local x, y, z
-    for j, land in ipairs(country:getchildren()) do
-        land:setmat({color='#3480C4', opacity = 0.6})
-        local points = land:getvertices()
-        x, y, z = points[1], points[2], points[3]
+    for j, land in ipairs(country:getchildren()) do                                         --traverse each land of a country
+        land:setmat({color='#3480C4', opacity = 0.6})                                       --set color of the land to light blue
+        local points = land:getvertices()                                                   --get all points of the land polygon
+        x, y, z = points[1], points[2], points[3]                                           --get the vertex coordinates
     end
-    local data = country:getdata()
-    local label = scene.addobj('label', {text = data['NAME'], color = 'black', size = 2})
-    label:setpos(x, y, z)
-    print(data['NAME'], ", POP:", data['POP_EST'], ", GDP:", data['GDP_MD_EST'])
+    local data = country:getdata()                                                          --get data of the country
+    local label = scene.addobj('label', {text = data['NAME'], color = 'black', size = 2})   --add a label of the country name
+    label:setpos(x, y, z)                                                                   --set the lable position
+    print(data['NAME'], ", POP:", data['POP_EST'], ", GDP:", data['GDP_MD_EST'])            --print out the other data
 end
-scene.render()
+scene.render()                                                                              --render the scene
