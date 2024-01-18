7c7a1327d594149f67c3b23bd49d8b6c159619ba
Mixwind <mixwind@gmail.com>
2024-01-18 01:30:45 +0000

    update lua/gltf.lua.
    Signed-off-by: Mixwind <mixwind@gmail.com>

--- lua/gltf.lua
+++ lua/gltf.lua
@@ -1,3 +1,3 @@
-scene.setenv({grid='plane'})
-local obj = scene.addobj('/res/2axle.glb')
-scene.render()
+scene.setenv({grid='plane'})                --add a plane grid to the scene
+local obj = scene.addobj('/res/2axle.glb')  --add a truct object
+scene.render()                              --render the scene
