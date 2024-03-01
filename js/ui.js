76f933cd04e488e9f1bcb5f0bca572effc381b8f
mixwind <mixwind@gmail.com>
2024-03-01 16:40:50 +0000

    test

--- js/ui.js
+++ js/ui.js
@@ -69,7 +69,7 @@ header.oncontextmenu = (e) => {
   e.preventDefault();
 }
 
-btns['play'].onclick = () => {
+btns['play'].onclick = async function (){
   scene.reload(); 
   if(!lua.engine) await new Promise(res => self.FinishModuleLoad = res); //如果模块没加载，等待模块加载完成
   runlua();
