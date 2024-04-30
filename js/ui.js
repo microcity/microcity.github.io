75cdae6b1ae14a1dec8baf806b98331d41f8042c
sunzhuo <mixwind@gmail.com>
2024-04-30 01:13:52 +0000

    test

--- js/ui.js
+++ js/ui.js
@@ -71,7 +71,7 @@ header.oncontextmenu = (e) => {
 
 btns['play'].onclick = async function (){
   let mediaQuery = window.matchMedia("(orientation: portrait), (max-width: 720px)");
-  if (mediaQuery.matches && aceeditor.getValue().includes("scene") && !btns['code'].active) {
+  if (mediaQuery.matches && aceeditor.getValue().includes("scene") && btns['code'].active) {
     btns['code'].onclick();
   }
   runlua();
